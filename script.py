8c96d814ef1b9c0608dfc3c340569b842ef9da15
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2021-03-10 20:19:19 +0100

    feat: Clean and Convert decision from html format to text format

--- script.py
+++ script.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
+from os import walk
 
 folder = 'data\\sortie 10-03-2021\\'
 output_folder = 'data\\cleaned 10-03-2021\\'
 
-f = open(folder + '1.html', 'r')
-file_content = f.read()
-f.close()
-soup = BeautifulSoup(file_content , features="html.parser")
-div = soup.find("div", {"id": "jurisprudence"})
-content = str(div.get_text())
-f = open(output_folder + '1.txt', 'w')
-f.write(content)
-f.close()
+_, _, filenames = next(walk(folder))
+
+for filename in filenames:
+    f = open(folder + filename, 'r')
+    file_content = f.read()
+    f.close()
+    soup = BeautifulSoup(file_content , features="html.parser")
+    div = soup.find("div", {"id": "jurisprudence"})
+    content = str(div.get_text())
+    f = open(output_folder + filename.split('.')[0] +'.txt', 'w', encoding='utf-8')
+    f.write(content)
+    f.close()
