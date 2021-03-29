a196992bf1d7b83c6646efa9b18c2cd7ba50be08
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2021-03-29 19:10:24 +0200

    fix: Create cleaned output folder if does not exist

--- script.py
+++ script.py
@@ -1,21 +1,24 @@
 from bs4 import BeautifulSoup
-from os import walk
+from os import walk, makedirs
+from os.path import exists
 from sys import argv
 
 for i in range(1,len(argv)):
     folder = 'data\\'+argv[i]+'\\'
     print(folder)
     output_folder = folder + 'cleaned\\'
+    if not exists(output_folder):
+        makedirs(output_folder)
 
     _, _, filenames = next(walk(folder))
 
     for filename in filenames:
-        f = open(folder + filename, 'r')
+        f = open(folder + filename, 'r', encoding='utf-8')
         file_content = f.read()
         f.close()
         soup = BeautifulSoup(file_content , features="html.parser")
         try:
-            div = soup.find("div", {"id": "jurisprudence"})
+            div = soup.find("div", {"id": "cCn" }) #"jurisprudence"})
             content = str(div.get_text())
         except Exception as e:
             print(filename)
